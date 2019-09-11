3786b3c04ab40386b0d578a637fe48a44c041063
Ryan Thorn <ryandthorn@gmail.com>
2019-09-11 08:22:23 -0700

    Update import

--- src/components/TodoInput.test.js
+++ src/components/TodoInput.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoInput from "./TodoInput";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<TodoInput />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
